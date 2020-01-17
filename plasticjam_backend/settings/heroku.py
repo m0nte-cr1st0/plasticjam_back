b34e4baeae023a8e9c67a5ae8c4f55d5190c2590
m0nte-cr1st0 <dinamo.mutu111@gmail.com>
2020-01-17 12:23:54 +0200

    added heroku settings file

--- plasticjam_backend/settings/heroku.py
+++ plasticjam_backend/settings/heroku.py
@@ -15,35 +15,3 @@ CORS_BLOCKED_URLS = ['user-detail']
 GENERATE_AUTO_DOCS = True
 
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
-
-LOGGING_FOLDER = os.path.abspath(os.path.join(BASE_DIR, '..', 'logs'))
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[%(name)s] %(levelname)s %(asctime)s\n%(message)s'
-        },
-    },
-    'handlers': {
-        # Send all messages to console
-        'filelog': {
-            'level': 'ERROR',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': os.path.join(LOGGING_FOLDER, 'plasticjam_django_errors.log'),
-            'when': 'D',
-            'interval': 7,
-            'backupCount': 4,
-        },
-    },
-    'loggers': {
-        # This is the "catch all" logger
-        'django': {
-            'handlers': ['filelog'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    }
-}
--- plasticjam_backend/settings/prod.py
+++ plasticjam_backend/settings/prod.py
@@ -3,34 +3,3 @@ from .base import *
 CORS_ORIGIN_ALLOW_ALL = True
 
 GENERATE_AUTO_DOCS = True
-
-LOGGING_FOLDER = config('GCL_LOGGING_FOLDER', default=os.path.abspath(os.path.join(BASE_DIR, '..', 'logs')))
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[%(name)s] %(levelname)s %(asctime)s\n%(message)s'
-        },
-    },
-    'handlers': {
-        # Send all messages to console
-        'filelog': {
-            'level': 'ERROR',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': os.path.join(LOGGING_FOLDER, 'gcl_django_errors.log'),
-            'when': 'D',
-            'interval': 7,
-            'backupCount': 4,
-        },
-    },
-    'loggers': {
-        # This is the "catch all" logger
-        'django': {
-            'handlers': ['filelog'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    }
-}
