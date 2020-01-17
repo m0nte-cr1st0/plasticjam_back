1b6001816c96c864efb59732336c5d1566781f29
m0nte-cr1st0 <dinamo.mutu111@gmail.com>
2020-01-17 12:58:56 +0200

    added heroku settings file

--- plasticjam_backend/settings/heroku.py
+++ plasticjam_backend/settings/heroku.py
@@ -9,7 +9,3 @@ DATABASES['default'].update(db_from_env)
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_BLOCKED_URLS = ['user-detail']
-
-GENERATE_AUTO_DOCS = True
-
-REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
