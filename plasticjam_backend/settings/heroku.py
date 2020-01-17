dbd9e256ddeaec05bd4229df2149f7f20ddadb0a
m0nte-cr1st0 <dinamo.mutu111@gmail.com>
2020-01-17 13:25:37 +0200

    added heroku settings file

--- plasticjam_backend/settings/heroku.py
+++ plasticjam_backend/settings/heroku.py
@@ -3,13 +3,9 @@ import dj_database_url
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = True
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_BLOCKED_URLS = ['user-detail']
-
-REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
