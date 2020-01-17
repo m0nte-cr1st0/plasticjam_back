c78b90c83acbb02091b1f7485db2e3d995fe3050
m0nte <cr1st0>
2020-01-17 04:11:29 +0200

    db adapter

--- plasticjam_backend/settings/base.py
+++ plasticjam_backend/settings/base.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'plasticjam_backend.wsgi.application'
 #         'PORT': '3306',
 #     }
 # }
-
+DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
