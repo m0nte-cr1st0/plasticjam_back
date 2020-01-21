98c5cec2484c932247272202e97f011cddac793c
m0nte <cr1st0>
2020-01-21 13:17:15 +0200

    fix date range filter

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -13,7 +13,6 @@ class UserStatisticFilter(filters.BaseFilterBackend):
     """
     Filters Statistic queryset by user, start_date and end_date params
     """
-
     def filter_queryset(self, request, queryset, view) -> StatisticQueryset:
         params = request.query_params
 
@@ -27,10 +26,15 @@ class UserStatisticFilter(filters.BaseFilterBackend):
             user = User.objects.get(pk=pk)
         except User.DoesNotExist:
             raise serializers.ValidationError('User not found')
-
         last_date = user.statistics.only('date').last().date
         start_date = request.query_params.get('start_date', last_date - datetime.timedelta(days=6))
         end_date = request.query_params.get('end_date', last_date)
+        if not end_date:
+            end_date = last_date
+        if isinstance(start_date, str):
+            start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+        if isinstance(end_date, str):
+            end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
         if start_date > end_date:
             start_date, end_date = end_date, start_date
         queryset = Statistic.objects.filter(user=user, date__gte=start_date, date__lte=end_date)
